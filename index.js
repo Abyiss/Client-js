e46367091f54dec002a0af9ca17aabf198e28e49
theodore ikehara <tikehara@ucsc.edu>
2022-02-06 22:51:09 -0800

    work in progress outline finished for index.js

--- index.js
+++ index.js
@@ -8,83 +8,159 @@
 const axios = require('axios').default;
 
 // This is the begining of the abyiss class
-class abyiss{
+class abyiss
+{
 
-    constructor(apiKey){
+    constructor(apiKey)
+    {
         this.axi = axios.create({
-          baseURL:  'https://api.abyiss.com',
-          headers:{'apiKey': apiKey}
+            baseURL : 'https://api.abyiss.com',
+            headers : {'apiKey' : apiKey}
         });
 
         this.ver = 'v1';
     }
 
     // This is the basic ping function
-    async ping(){
+    async ping()
+    {
         return await this.axi.get('/ping')
-        .then(function(res){
-            console.log(res.data)
-            return res.data
-        })
-        .catch(function(err){
-            console.log(err)
-            return err
-        })
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
     }
 
     // These two functions get exchange data
-    async get_all(){
-        return await this.axi.get(this.ver+'/exchanges')
-        .then(function(res){
-            console.log(res.data)
-            return res.data
-        })
-        .catch(function(err){
-            console.log(err)
-            return err
-        })
+    async get_all()
+    {
+        return await this.axi.get(this.ver + '/exchanges')
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
     }
-    async get_exchange(exchange){
-        return await this.axi.get(this.ver+'/'+exchange)
-        .then(function(res){
-            console.log(res.data)
-            return res.data
-        })
-        .catch(function(err){
-            console.log(err)
-            return err
-        })
-    }
-
-    async get_status(exchange){
-        return await this.axi.get(this.ver+'/'+exchange+'/status')
-        .then(function(res){
-            console.log(res.data)
-            return res.data
-        })
-        .catch(function(err){
-            console.log(err)
-            return err
-        })
+    async get_exchange(exchange)
+    {
+        return await this.axi.get(this.ver + '/' + exchange)
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
     }
 
-    async get_markets(exchange){
-        return await this.axi.get(this.ver+'/'+exchange+'/markets')
-        .then(function(res){
-            console.log(res.data)
-            return res.data
-        })
-        .catch(function(err){
-            console.log(err)
-            return err
-        })
+    async get_status(exchange)
+    {
+        return await this.axi.get(this.ver + '/' + exchange + '/status')
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
     }
 
-// This is the end of the abyiss class
+    async get_markets(exchange)
+    {
+        return await this.axi.get(this.ver + '/' + exchange + '/markets')
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
+    }
+
+    async get_markets_details(exchange, market)
+    {
+        return await this.axi.get(this.ver + '/' + exchange + '/' + market)
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
+    }
+
+    // market data
+    async aggregates(exchange, market, time)
+    {
+        return await this.axi
+            .get(this.ver + '/' + exchange + '/' + market + '/aggregates/' +
+                 time + 'm')
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
+    }
+
+    async trades(exchange, market)
+    {
+        return await this.axi
+            .get(this.ver + '/' + exchange + '/' + market + '/trades')
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
+    }
+
+    async quotes(exchange, market)
+    {
+        return await this.axi
+            .get(this.ver + '/' + exchange + '/' + market + '/quotes')
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
+    }
+
+    async orders(exchange, market)
+    {
+        return await this.axi
+            .get(this.ver + '/' + exchange + '/' + market + '/orders')
+            .then(function(res) {
+                console.log(res.data)
+                return res.data
+            })
+            .catch(function(err) {
+                console.log(err)
+                return err
+            })
+    }
+
+    // This is the end of the abyiss class
 }
 
 let ax = new abyiss('d1V-97Qg@Lh6xuakUf2e)J^gE(@!*Xo5i06');
-ax.get_markets('coinbasepro');
-
-
+ax.orders('coinbasepro', 'BTC-USD');
 
